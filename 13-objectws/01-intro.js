09ce6e6dfb960a143300782b9e4ec1814343ea7a
mhishmeh <hishmoe@gmail.com>
2025-03-08 11:36:24 -0600

    finished homework5

---
+++ 13-objectws/01-intro.js
@@ -0,0 +1,12 @@
+const obj1 = new Object()
+const obj2 = {}
+
+const mouse = {
+    name: 'HP smart',
+    price: 10.99,
+    quantity: 15,
+    delivery: ['pick up', 'delivery']
+};
+
+// add colors
+mouse.colors = ['white','Black','Blue','Pink']
--- homework/homework05.js
+++ homework/homework05.js
@@ -1,5 +1,191 @@
-console.log(`\n--------------- Task Ten ---------------\n`);
+console.log(`\n--------------- Task One ---------------\n`);
+/*
+Write a function named countPos() which takes an array of numbers as an argument and returns how many elements are positive​ when invoked. 
 
+*/
+
+function countPos(arr) {
+    return arr.filter((x) => x > 0).length
+}
+console.log(countPos([3,4,0,-1,9]))
+
+console.log(`\n--------------- Task Two ---------------\n`);
+/*
+
+Write a function named countA() which takes a string argument and returns how many A or a there are in the given string when invoked.
+
+*/
+
+function countA(str) {
+return (str.match(/[Aa]/g) || []).length
+}
+
+console.log(countA('heAAaaaaaaaaAa'))
+
+
+
+console.log(`\n--------------- Task Three ---------------\n`);
+/*
+
+Write a function named as countVowels() which takes a string word as an argument and returns the count of the vowel letters when invoked.
+*/
+
+function countVowels(str) {
+    return (str.match(/[AEIOUaeiou]/g)|| []).length
+
+}
+console.log(countVowels('heyee'))
+
+
+console.log(`\n--------------- Task Four ---------------\n`);
+
+/*
+
+Write a function named as countConsonants() which takes a string word as an argument and returns the count of the consonant letters when invoked.
+
+*/
+
+function countConsonants(str) {
+    return (str.match(/[^AEIOUaeiou]/g)|| []).length
+}
+
+console.log(countConsonants(''))
+
+
+
+
+
+
+console.log(`\n--------------- Task Five ---------------\n`);
+
+/*
+Write a function named countWords() which takes a string argument and returns the total count of words in the given string when invoked.
+
+NOTE: Be careful about the extra whitespaces before and after the string.
+*/
+
+function countWords(arr) {
+    const arrOfStr = arr.trim().split(' ')
+    return arrOfStr.length
+}
+
+console.log(countWords('Cypress is an UI automation tool.'))
+
+
+
+console.log(`\n--------------- Task Six ---------------\n`);
+
+/*
+
+Write a function named as factorial() which takes a number as an argument and returns the factorial of the number when invoked.
+
+NOTE: Mathematically, the factorial of a non-negative integer n is defined as:
+n! = n × (n-1) × (n-2) × ... × 2 × 1
+
+Assume you will not be given a negative number.
+
+
+*/
+
+function factorial(num) {
+    let product = 1
+    for (let i = num; i >= 1; i--) {
+        product *= i
+        
+    }
+    return product
+}
+
+console.log(factorial(3))
+
+
+console.log(`\n--------------- Task Seven ---------------\n`);
+/*
+
+Write a function named as isPalindrome() which takes a string word as an argument and returns true if the word is palindrome or returns false otherwise when invoked.
+
+NOTE: Palindrome: It is a word that is read the same backward as forward
+Examples: kayak, civic, madam
+
+NOTE: your function should ignore case sensitivity
+
+*/
+
+function isPalindrome(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() !== str[str.length - i - 1].toLowerCase()) return false
+    }
+    return true
+}
+console.log(isPalindrome('OMARRAMO'))
+
+
+
+console.log(`\n--------------- Task Eight ---------------\n`);
+/*
+
+Write a function named as countMultipleWords() which takes an array as an argument and returns the count of the elements that has multiple words when invoked.
+
+Examples:
+countMultipleWords([ "foo", "", "    ", "foo bar", "     foo" ]) 		-> 1
+countMultipleWords([ "foo", "bar", "foobar", "     foobar   " ]) 		-> 0
+countMultipleWords([ "f o o", "b a r", "foo bar", "     foo bar   " ]) 	-> 4
+countMultipleWords([ ]) 	
+
+*/
+
+function countMultipleWords(arr) {
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i].trim().split(' ')
+        if (element.length > 1) count++
+    }
+    return count
+}
+
+console.log(countMultipleWords([ ]) )
+
+
+
+
+
+console.log(`\n--------------- Task Nine ---------------\n`);
+
+/*
+
+Write a function named as count3OrLess() which takes a string word as an argument and returns the count of the words that has 3 characters or less when invoked.
+
+
+*/
+
+function count3OrLess(str) {
+    const arr = str.split(' ')
+    let count = 0
+    for (let i of arr) {
+        if (i.length <= 3) count++
+    }
+    return count
+}
+
+console.log(count3OrLess('JavaScript is fun'))
+
+
+
+
+
+
+console.log(`\n--------------- Task Ten ---------------\n`);
+function isPrime(num) {
+    if (num <= 1) return false
+    if (num === 2) return true
+
+    for (let i = 3; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false
+    }
+    return true
+}
+
+console.log(isPrime(25))
 
 
 
